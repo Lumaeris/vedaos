4aa97240ee1779043c2d5b5abfd0db0380d0361b
Jill Fiore <contact@lumaeris.com>
2025-09-30 18:15:35 +0700

    feat: add deck image and install lsfg-vk to desk and deck

---
+++ files/scripts/install-lsfgvk.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# this script is somewhat based on https://github.com/askpng/solarpowered/blob/main/files/scripts/base/bazzite.sh
+
+set -ouex pipefail
+
+LSFG_TAG=$(curl --fail --retry 5 --retry-delay 5 --retry-all-errors -s https://api.github.com/repos/PancakeTAS/lsfg-vk/releases | grep tag_name | cut -d : -f2 | tr -d 'v", ' | head -1)
+
+echo 'Installing lsfg-vk.'
+dnf5 install -y \
+    https://github.com/PancakeTAS/lsfg-vk/releases/download/v$LSFG_TAG/lsfg-vk-$LSFG_TAG.x86_64.rpm
