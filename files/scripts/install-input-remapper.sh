537edb59e633f7c304abb2aa5df7efdd964a4066
Jill Fiore <contact@lumaeris.com>
2025-10-29 10:27:38 +0700

    fix(desktop): replace input remapper with upstream-git
    current one is not compatible I think

---
+++ files/scripts/install-input-remapper.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+set -oue pipefail
+
+dnf5 -y remove input-remapper
+
+dnf5 -y --setopt=install_weak_deps=False install gtksourceview4 python3-devel python3-evdev python3-pydantic python3-psutil python3-setuptools python3-pydbus
+
+pushd . > /dev/null
+dir="$(mktemp -d)"
+cd "${dir}"
+git clone --depth=1 https://github.com/sezanzeb/input-remapper.git
+cd input-remapper
+python3 setup.py install
+popd > /dev/null
+rm -rf "${dir}"
