b0baf3d1201bc718e76b720cfdabffd7463b1021
Jill Fiore <contact@lumaeris.com>
2025-11-03 21:53:13 +0700

    fix(desktop): rename bazaar config and remove leftover script

--- files/scripts/so-bazaar.sh
+++ files/scripts/so-bazaar.sh
@@ -4,5 +4,6 @@
 set -oue pipefail
 
 mv /usr/share/ublue-os/bazaar /etc
-sed -i 's|/usr/share/ublue-os/|/run/host/etc/|g' /etc/bazaar/content.yaml
+mv /etc/bazaar/content.yaml /etc/bazaar/config.yaml
+sed -i 's|/usr/share/ublue-os/|/run/host/etc/|g' /etc/bazaar/config.yaml
 sed -i '/io\.github\.kolunmi\.Bazaar/d' /etc/bazaar/blocklist.txt
