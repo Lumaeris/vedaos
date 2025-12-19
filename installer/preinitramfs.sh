10f22c9edb03d4dc41564ed84b6aacd7a131e126
Jill Fiore <contact@lumaeris.com>
2025-12-19 21:58:18 +0700

    Revert "chore: replace titanoboa with build-container-installer"
    This reverts commit d9f0fe4fe96268727bebb5840cf658b0035180dd.

---
+++ installer/preinitramfs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -exo pipefail
+
+# Swap kernel with vanilla and rebuild initramfs.
+cachy_kernel_pkgs=(
+    kernel-cachyos
+    kernel-cachyos-core
+    kernel-cachyos-modules
+    kernel-cachyos-devel-matched
+)
+stock_kernel_pkgs=(
+    kernel
+    kernel-core
+    kernel-modules
+    kernel-modules-core
+)
+rpm --erase -v --nodeps "${cachy_kernel_pkgs[@]}"
+dnf -yq install "${stock_kernel_pkgs[@]}"
+
+imageref="$(podman images --format '{{ index .Names 0 }}\n' 'vedaos*' | head -1)"
+imageref="${imageref##*://}"
+imageref="${imageref%%:*}"
+
+# Include nvidia-gpu-firmware package.
+dnf install -yq nvidia-gpu-firmware || :
+dnf clean all -yq
