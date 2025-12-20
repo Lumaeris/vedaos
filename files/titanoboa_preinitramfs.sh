192a95a2a7264c08d15b5d8930afbe33d8cbf82c
Jill Fiore <contact@lumaeris.com>
2025-12-20 15:05:37 +0700

    fix: add workarounds in preinitramfs for titanoboa
    guess I have to do it

--- files/titanoboa_preinitramfs.sh
+++ files/titanoboa_preinitramfs.sh
@@ -15,8 +15,24 @@ stock_kernel_pkgs=(
     kernel-modules-core
 )
 rpm --erase -v --nodeps "${cachy_kernel_pkgs[@]}"
+
 rm -rf /usr/lib/modules/*
+# create a shims to bypass kernel install triggering dracut/rpm-ostree
+# seems to be minimal impact, but allows progress on build
+pushd /usr/lib/kernel/install.d
+mv 05-rpmostree.install 05-rpmostree.install.bak
+mv 50-dracut.install 50-dracut.install.bak
+printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
+printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
+chmod +x 05-rpmostree.install 50-dracut.install
+popd
+
 dnf -yq install "${stock_kernel_pkgs[@]}"
+
+pushd /usr/lib/kernel/install.d
+mv -f 05-rpmostree.install.bak 05-rpmostree.install
+mv -f 50-dracut.install.bak 50-dracut.install
+popd
 
 imageref="$(podman images --format '{{ index .Names 0 }}\n' 'vedaos*' | head -1)"
 imageref="${imageref##*://}"
